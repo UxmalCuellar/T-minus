983503c1a5747eb21bc30118f6bc98c9ded97852
Uxmal Cuellar <uxmal.c@gmail.com>
2020-03-02 14:37:36 +1100

    Cleaned up code removed focus outline

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const {ipcRenderer} = require('electron');
+const { ipcRenderer } = require('electron');
 const electron = require('electron');
 const path = require('path');
 const BrowserWindow = electron.remote.BrowserWindow;
@@ -6,7 +6,7 @@ const BrowserWindow = electron.remote.BrowserWindow;
 var dateToCountDown
 var timerId;
 
-ipcRenderer.on('run-insert-dates', function(event, arg) {
+ipcRenderer.on('run-insert-dates', function (event, arg) {
     // fill select tag with options and values
     document.getElementById("dateOpt").innerHTML = arg
 })
@@ -14,7 +14,7 @@ ipcRenderer.on('run-insert-dates', function(event, arg) {
 function updateTimer() {
     // stops and removes the current setinterval (timer)
     console.log("Removing id:", timerId);
-    clearInterval(timerId);    
+    clearInterval(timerId);
 }
 
 function abortTask() {
@@ -22,7 +22,7 @@ function abortTask() {
     console.log("Removing id(abort):", timerId);
     document.getElementById("timer").innerHTML = "00:00:00"
     document.getElementById("dateOpt").selectedIndex = 0;
-    clearInterval(timerId);    
+    clearInterval(timerId);
 }
 
 function formatEventName(str) {
@@ -38,8 +38,8 @@ function formattedDateTime(str) {
     return newStr;
 }
 
- function startTimer() {
-     // BUG previous timer does not get cancelled sometimes
+function startTimer() {
+    // BUG previous timer does not get cancelled sometimes
     // Gets users selection and begins countdown 
     var val = document.getElementById('dateOpt');
     var x = val.options[val.selectedIndex].value;
@@ -47,13 +47,13 @@ function formattedDateTime(str) {
     var y = text.options[text.selectedIndex].text;
     var formatName = formatEventName(y);
     var formatDate = formattedDateTime(x);
-    
+
     console.log(x)
     document.getElementById('eventDetails').innerHTML = formatDate;
     document.getElementById('eventName').innerHTML = formatName;
     dateToCountDown = x
 
-    if(dateToCountDown == '') {
+    if (dateToCountDown == '') {
         dateToCountDown = new Date().getTime();
         document.getElementById("timer").innerHTML = "00:00:00"
         console.log('Date not selected');
@@ -62,23 +62,23 @@ function formattedDateTime(str) {
 
         var countDownDateTime = new Date(dateToCountDown).getTime();
 
-        timerId = setInterval(function() {
+        timerId = setInterval(function () {
 
             var now = new Date().getTime();
 
             var difference = countDownDateTime - now;
             var days = Math.floor(difference / (1000 * 60 * 60 * 24));
             var hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
-            var minutes = Math.floor((difference / 1000 / 60 ) % 60);
+            var minutes = Math.floor((difference / 1000 / 60) % 60);
             var seconds = Math.floor((difference / 1000) % 60);
 
             document.getElementById("timer").innerHTML = isDaysOrDay(days) + addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds);
 
-            if(difference < 0) {
+            if (difference < 0) {
                 clearInterval(timerId);
                 document.getElementById("timer").innerHTML = "00:00:00"
             }
-    console.log("New timerId created", timerId);
+            console.log("New timerId created", timerId);
         }, 1000);
     }
     console.log("finished running timerId", timerId);
@@ -86,10 +86,10 @@ function formattedDateTime(str) {
 
 function isDaysOrDay(d) {
     // check how many days are left in timer and return appropriate grammar
-    if(d == 0) {
-    return ("")
-    } 
-    return ( d == 1 ? d + " day\n" : d + " days\n")
+    if (d == 0) {
+        return ("")
+    }
+    return (d == 1 ? d + " day\n" : d + " days\n")
 }
 
 function addZero(n) {
@@ -106,5 +106,5 @@ function formatDates() {
     // converts selected date to RFC3339 format
     var input = document.getElementById("fromDate").value;
     var dateEntered = new Date(input);
-    console.log(dateEntered.toISOString()); 
+    console.log(dateEntered.toISOString());
 }
